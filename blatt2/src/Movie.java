5feaae1c677fe76b994af62361320157af7c0809
Soloninov Aleksandr <stenkin92@yahoo.com>
2015-10-22 01:06:33 +0200

    update blatt 2

---
+++ blatt2/src/Movie.java
@@ -0,0 +1,110 @@
+/** Movie Class
+ * ISW 15|16
+ * 
+ * @autor Soloninov Aleksandr
+ * @version 1.2.18   
+ */
+
+public class Movie {
+  //private stack
+  private String title;
+  private int time;
+  private int number;
+  private Director director;
+  private Customer customer;
+  //public stack
+  private static int nextNumber; // Nummer des Objekts und erhöht die Klassenvariable
+  
+  Movie(String getTitle, int getTime) { 
+
+    this.time = getTime; 
+    this.title = getTitle;
+    this.number = nextNumber++;
+  } // end Constructor
+
+  public String getTitle() {
+    return title;
+  } // end getTitle()
+
+  public void setTitle(String title) {
+    this.title = title;  //
+  } // end setTitle(String title)
+
+  /* Die "this" zeigt immer auf das aktuelle Objekt der klasse.
+    Bsp : wie hier, this.title zeigt auf klassevariable, "title" nun auf locale variable
+    */
+
+  public int getTime() {
+    return time;
+  } // end getTime()
+
+  public void setTime(int time) {
+    this.time = time;
+  }
+
+  public Director getDirector() {
+    return this.director;
+  }
+
+  public void setDirector(Director director) {
+    this.director = director;
+  }
+
+  public Customer getCustomer() {
+    return this.customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+  
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public static int getNextNumber() {
+    return nextNumber;
+  }
+
+  public static void setNextNumber(int getNextNumber) {
+    nextNumber = getNextNumber;
+  }
+  
+  /** showInformation
+   * ISW 15|16
+   * 
+   * @autor Soloninov Aleksandr
+   * @version 2.2.8   
+   */
+  
+  public void showInformation() {
+    System.out.println("Title : " + this.title);
+    System.out.println("time : " + this.time);
+    System.out.println("Number : " + number);
+    System.out.println("nextNumber : " + nextNumber);
+  }
+
+  // bracuht man zum con @ to string
+  public String toString() {
+    return this.title + " is over " + this.time + " minutes long.";
+  }
+  
+  /** Main
+   * 
+   * @autor Soloninov Aleksandr
+   * @version 1.0.4   
+   */
+  public static void main(String[] args) {
+    final Movie zero;
+    zero = new Movie("zero", 0);
+    zero.showInformation();
+    
+    final Movie one = new Movie("one",1);
+    one.showInformation();
+  }
+}
+
---
+++ blatt2/src/MyFirstMovieManager.java
@@ -0,0 +1,157 @@
+/** Soloninov Aleksandr, ISW WS 15|16
+ * Blatt 1, Aufgabe 1.3
+ * Relase 1.2.37
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class MyFirstMovieManager {
+  private final Map<Movie, ArrayList<Performer>> map;
+
+  public  MyFirstMovieManager() {
+    this.map = new HashMap<Movie, ArrayList<Performer>>();// Erzeugen die Map
+  } // Constructor
+  
+  /** void addPerformer(Movie getMovie, Performer getPerformer)
+   * Blatt 1, Aufgabe 1.3
+   * Relase v1.0.7
+   */
+  
+  public void addPerformer(Movie getMovie, Performer getPerformer)  {
+    List<Performer> performers = this.map.get(getMovie);           //
+    if  (performers == null)  {
+      // wenn keinen prf, erzugen nen neuen
+      performers = new ArrayList<Performer>();
+    }
+
+    if  (!performers.contains(getPerformer))  {
+      performers.add(getPerformer);
+      this.map.put(getMovie, (ArrayList<Performer>)performers);
+    }
+  }
+  
+  /** public void clearPerformers(Movie getMovie)
+   * Blatt 1, Aufgabe 1.3
+   * Relase v1.0.7
+   */
+
+  public void clearPerformers(Movie getMovie) {
+    final List<Performer> performers = this.map.get(getMovie);
+    if  (performers != null)  {
+      performers.clear();
+      this.map.put(getMovie, (ArrayList<Performer>)performers);
+    }
+  }
+  // gibt alle performers ins Movie
+  public List<Performer> getPerformers(Movie getMovie)  {
+    return this.map.get(getMovie);
+  }
+  
+  /** public void clearPerformers(Movie getMovie)
+   * Blatt 1, Aufgabe 1.3
+   * Relase v1.0.9
+   */
+  
+  public Performer getPerformer(Movie movie, int ii) {
+    final List<Performer>  performers = this.getPerformers(movie);
+    if  (performers != null && performers.size() > ii )  {
+      return performers.get(ii);
+    } else {
+      return null;
+    }
+  }
+  /** getOutstandingPerformers(Movie movie)
+   * Blatt 1, Aufgabe 1.3
+   * Relase v1.0.42
+   */
+  public List<Performer> getOutstandingPerformers(Movie movie)  {
+    final List<Performer> performers = this.getPerformers(movie);
+
+    final  List<Performer> returnList = new ArrayList<Performer>();
+    for (Performer performer : performers)  {
+      if  (performer.isOutstanding()) {
+        returnList.add(performer);
+      }
+    }
+    return returnList;
+  }
+  /** searchPerformer(Movie getMovie, String byName)
+   * Blatt 1, Aufgabe 1.3
+   * Relase v1.0.48
+   */
+  public Boolean searchPerformer(Movie getMovie, String byName) {
+    final List<Performer> performers = this.getPerformers(getMovie);
+    for (Performer performer : performers)  {
+      if  (performer.getFirstname().equalsIgnoreCase(byName)
+          &&  performer.getLastname().equalsIgnoreCase(byName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  /** main
+   * Relase v1.0.93
+   */
+  
+  public static void main(String[] args)  {
+    final MyFirstMovieManager manager = new MyFirstMovieManager();
+    final Movie futurama = new Movie("Futurama", 100500);
+    System.out.println(futurama);
+    final Performer fry = new Performer("Philip J.", "Fry", Gender.MALE);
+    final Performer leela = new Performer("Turanga", "Leela", Gender.FEMALE);
+    final Performer bender = new Performer("Bieger Rodriguez", "Bender ", Gender.MALE);
+    final Performer zoidberg = new Performer("Dr. John", "Zoidberg", Gender.MALE);
+    final Performer conrad = new Performer("Hermes", "Conrad", Gender.MALE);
+    final Performer amy = new Performer("Wong", "Amy", Gender.FEMALE);
+    
+    bender.setOutstanding(true);
+    zoidberg.setOutstanding(true);
+
+    manager.addPerformer(futurama, fry);
+    manager.addPerformer(futurama, leela);
+    manager.addPerformer(futurama, bender);
+    manager.addPerformer(futurama, zoidberg);
+    manager.addPerformer(futurama, conrad);
+    manager.addPerformer(futurama, amy);
+    
+    System.out.println(manager.getPerformers(futurama));
+    System.out.println("Outstanding Performers : " + manager.getOutstandingPerformers(futurama));
+
+    leela.setLastname(fry.getLastname());
+    System.out.println("Fry + Leela  = "  +  leela.getLastname() + " " + leela.getFirstname());
+
+    if  (manager.searchPerformer(futurama, "Vader").equals(futurama) ) { 
+      System.out.println("Performer not in this movie" ); 
+    } else  { 
+      System.out.println("Performer is in this movie"); 
+    }
+    System.out.println("Futurama is closed  :( ");
+    manager.clearPerformers(futurama);
+    System.out.println("0 Performers " + manager.getPerformers(futurama));
+  }
+}
+
+/*
+ ArrayList:
+ >> Nicht direkte Map >>  Zuordnung muesste extern geschehen
+ >> Ein Array -> Schneller Indexzugriff
+ >> Langsame Suche nach Schluesseln
+
+  
+ HashSet:
+ >> Implementiert eine gehashte Menge >>  Schneller Schluesselzugriff
+ >> Nicht direkte Map >>  Zuordnung muesste extern geschehen
+ >> Keine Reihenfolge definierbar
+
+  
+ HashMap:
+ >> Suche und Zugriff  sind langsam
+ >> Gehashte Zuordnung >>  Schneller Schluesseszugriff
+ >> Keine Reihenfolge definierbar
+ */
