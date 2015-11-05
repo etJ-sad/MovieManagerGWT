c3912110447f683511fb8bb3f0f7f8b56ac247d0
Soloninov Aleksandr <stenkin92@yahoo.com>
2015-11-05 01:48:56 +0100

    big update, add : @textedit, @lbox, @tbox, @path addButton
    @version

--- NDA/src/de/unihd/movies/client/MovieManager.java
+++ NDA/src/de/unihd/movies/client/MovieManager.java
@@ -10,44 +10,41 @@ import java.util.LinkedList;
 import java.util.List;
 import com.google.gwt.core.client.EntryPoint;
 
-
-
-
-
 /**
  * The Class MovieManager.
  */
 
 public class MovieManager  implements EntryPoint {
 	
-	public final List<String> LANG = Arrays.asList("German", "English", "Spanish", "France", "Russish");
-  
+	
+	
 	LinkedList<Movie> getList() {
-	    LinkedList<Movie> list = new LinkedList<Movie>();
-		
-				list.add(new Movie (1,	"Star Wars I",	166, "English", "First Film new Trilogy", "space"));
-				list.add(new Movie (1,	"Star Wars I",	166, "English", "First Film new Trilogy", "space"));
-				list.add(new Movie (3,	"Star Wars III",211, "English", "Three Film new Trilogy", "space"));
-				list.add(new Movie (4,	"Star Wars IV",	149, "English", "First Film old Trilogy", "space"));
-				list.add(new Movie (5,	"Star Wars V",	154, "English", "Second Film old Trilogy", "space"));
-				list.add(new Movie (6,	"Star Wars VI",	171, "English", "Three Film old Trilogy", "space"));  
+	    LinkedList<Movie> list = new LinkedList<Movie>();		
+	    list.add(new Movie (1,	"Star Wars I",	166, "English", "First Film new Trilogy", "space"));
+	    list.add(new Movie (2,	"Star Wars I",	166, "English", "First Film new Trilogy", "space"));
+	    list.add(new Movie (3,	"Star Wars III",211, "English", "Three Film new Trilogy", "space"));
+	    list.add(new Movie (4,	"Star Wars IV",	149, "English", "First Film old Trilogy", "space"));
+	    list.add(new Movie (5,	"Star Wars V",	154, "English", "Second Film old Trilogy", "space"));
+	    list.add(new Movie (6,	"Star Wars VI",	171, "English", "Three Film old Trilogy", "space"));  
 				
 	    return list;
 	}
 	
+	public final List<String> LANG = Arrays.asList("German", "English", "Spanish", "France", "Russish");
+  	
 	LinkedList<Movie> empty() {
 	    LinkedList<Movie> list = new LinkedList<Movie>();
 			list.add(new Movie (0,	"",	0, "", "", "")); 
 			return list;
 	}
-		
+			
 	public void onModuleLoad() {
-			  MovieUI run = new MovieUI();
-			  run.createTable();
-			  run.setTable();
-			  run.addButton();
-			  run.delButton();
-			  run.show();
-		}
+		MovieUI runModule = new MovieUI();
+		runModule.setupColumn();
+		runModule.createTable();
+		runModule.addButton();
+		runModule.delButton();
+		runModule.show();
+	}
 }
 
--- NDA/src/de/unihd/movies/client/MovieUI.java
+++ NDA/src/de/unihd/movies/client/MovieUI.java
@@ -4,38 +4,22 @@
 
 package de.unihd.movies.client;
 
-
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
 
-import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.cell.client.SelectionCell;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.cellview.client.CellTable;
-
+import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.TextColumn;
-import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
 import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
-import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.Window;
-
 import com.google.gwt.user.client.ui.*;
-
-
-
-
 
 public class MovieUI extends Composite {
 
@@ -46,32 +30,18 @@ public class MovieUI extends Composite {
 	
 	final ListDataProvider<Movie> dataProvider = new ListDataProvider<Movie>(manager.getList());
     final SingleSelectionModel<Movie> selectionModel = new SingleSelectionModel<Movie>();
-    
+    SelectionCell categoryCell = new SelectionCell(manager.LANG);
+
 	public final TextBox tbox = new TextBox();
-    public final ListBox lbox = new ListBox();
+    
     
 	int numRows = table.getRowCount();
 	int ids;
-	
-
-	class CustomEditCell{
-	    
-	    public void render(com.google.gwt.cell.client.Cell.Context context,
-	            String value, SafeHtmlBuilder sb) {
-	        // context.getColumn()==2 indicate Record ID column and context.getIndex()==0 indicate non editable cell in 1st empty row
-	        if(context.getColumn()==2 &&    ( context.getIndex()==0  || context.getIndex()%10 == 0)){
-	            sb.appendHtmlConstant("<div contentEditable='false' unselectable='true'></div>");
-	        }else{
-	        render(context, value, sb);
-	        }
-
-	    }
-	}
-	
+		
     //-------------------------------------------//
     
-    public void createTable(){
-    	//id 
+    public void setupColumn(){ 	
+       	//id 
 	    TextColumn<Movie> idColumn = new TextColumn<Movie>() {
 		      @Override
 		      public String getValue(Movie object) {
@@ -81,7 +51,7 @@ public class MovieUI extends Composite {
 		    }; table.addColumn(idColumn, "id");
 	    
 		//Name
-	    TextColumn<Movie> nameColumn = new TextColumn<Movie>() {
+	    Column<Movie,String> nameColumn = new Column<Movie,String>( new EditTextCell()) {
 	      @Override
 	      public String getValue(Movie object) {
 	        return object.getName();
@@ -89,7 +59,7 @@ public class MovieUI extends Composite {
 	       }; table.addColumn(nameColumn, "Name");
 	    
 	    //Time
-	    TextColumn<Movie> timeColumn = new TextColumn<Movie>() {
+	    Column<Movie, String> timeColumn = new Column<Movie, String>( new EditTextCell()) {
 		      @Override
 		      public String getValue(Movie object) {
 		    	  String buffer = String.valueOf(object.getTime());
@@ -98,7 +68,7 @@ public class MovieUI extends Composite {
 		    }; table.addColumn(timeColumn, "Time");
 		
 		//Language
-	    TextColumn<Movie> languageColumn = new TextColumn<Movie>() {
+	    Column<Movie,String> languageColumn = new Column<Movie,String>( categoryCell) {
 		      @Override
 		      public String getValue(Movie object) {
 		        return object.getLanguage();
@@ -106,7 +76,7 @@ public class MovieUI extends Composite {
 		    }; table.addColumn(languageColumn, "Language");
 		
 		//Description
-	    TextColumn<Movie> descriptionColumn = new TextColumn<Movie>() {
+	    Column<Movie,String> descriptionColumn = new Column<Movie, String>( new EditTextCell()) {
 		      @Override
 		      public String getValue(Movie object) {
 		        return object.getDescription();
@@ -115,7 +85,7 @@ public class MovieUI extends Composite {
 	    
 	    
 	    //Place
-	    TextColumn<Movie> placeColumn = new TextColumn<Movie>() {
+	    Column<Movie,String> placeColumn = new Column<Movie, String>( new EditTextCell()) {
 	      @Override
 	      public String getValue(Movie object) {
 	        return object.getPlace();
@@ -123,7 +93,7 @@ public class MovieUI extends Composite {
 	    }; table.addColumn(placeColumn, "Place");
 	    
 	    
-	 // setSortable
+	    //	setSortable
 	    idColumn.setSortable(true);
 	    nameColumn.setSortable(true);
 	    timeColumn.setSortable(true);
@@ -131,87 +101,79 @@ public class MovieUI extends Composite {
 	    descriptionColumn.setSortable(true);
 	    placeColumn.setSortable(true);
 	    
-		  //Handler
-			ListHandler<Movie> columnSortHandler = new ListHandler<Movie>(
-					dataProvider.getList()
-			);
+	    //Handler
+	    ListHandler<Movie> columnSortHandler = new ListHandler<Movie>(
+	    		dataProvider.getList()
+	    		);
 
-			// Sorts:
+	    // Sorts:
 			
-			// idColumn sort
-			columnSortHandler.setComparator(idColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					if(m1.getId() > m2.getId()) return 1;
-					if(m1.getId() < m2.getId()) return -1;
-					return 0;
+	    // idColumn sort
+	    columnSortHandler.setComparator(idColumn, new Comparator<Movie>() {
+	    	@Override
+	    	public int compare(Movie m1, Movie m2) {
+	    		if(m1.getId() > m2.getId()) return 1;
+	    		if(m1.getId() < m2.getId()) return -1;
+	    		return 0;
 				};
-			});
+		});
 			
-			// nameCoulmn sort	
-			columnSortHandler.setComparator(nameColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					return (m1.getName().compareTo(m2.getName()));
-				};
-			});
+		// nameCoulmn sort	
+		columnSortHandler.setComparator(nameColumn, new Comparator<Movie>() {
+		@Override
+		public int compare(Movie m1, Movie m2) {
+			return (m1.getName().compareTo(m2.getName()));
+			};
+		});
+		
+		// timeColumn sort
+		columnSortHandler.setComparator(timeColumn, new Comparator<Movie>() {
+		@Override
+		public int compare(Movie m1, Movie m2) {
+			if(m1.getId() > m2.getId()) return 1;
+			if(m1.getId() < m2.getId()) return -1;
+			return 0;
+			};
+		});
 			
-			// timeColumn sort
-			columnSortHandler.setComparator(timeColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					if(m1.getId() > m2.getId()) return 1;
-					if(m1.getId() < m2.getId()) return -1;
-					return 0;
-				};
-			});
+		// languageColumn sort
+		columnSortHandler.setComparator(languageColumn, new Comparator<Movie>() {
+		@Override
+		public int compare(Movie m1, Movie m2) {
+			return (m1.getName().compareTo(m2.getName()));
+			};
+		});
 			
-			// languageColumn sort
-			columnSortHandler.setComparator(languageColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					return (m1.getName().compareTo(m2.getName()));
-				};
-			});
+		// descriptionColumn sort
+		columnSortHandler.setComparator(descriptionColumn, new Comparator<Movie>() {
+		@Override
+		public int compare(Movie m1, Movie m2) {
+			return (m1.getName().compareTo(m2.getName()));
+			};
+		});
 			
-			// descriptionColumn sort
-			columnSortHandler.setComparator(descriptionColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					return (m1.getName().compareTo(m2.getName()));
-				};
-			});
-			
-			// placeColumn Sort
-			
-			columnSortHandler.setComparator(placeColumn, new Comparator<Movie>() {
-				@Override
-				public int compare(Movie m1, Movie m2) {
-					return (m1.getName().compareTo(m2.getName()));
-				};
-			});
-			
-			table.addColumnSortHandler(columnSortHandler);
+		// placeColumn Sort
+		columnSortHandler.setComparator(placeColumn, new Comparator<Movie>() {
+		@Override
+		public int compare(Movie m1, Movie m2) {
+			return (m1.getName().compareTo(m2.getName()));
+			};
+		});
+		table.addColumnSortHandler(columnSortHandler);
     }
+
     
-    //(table,10,140);
-	public void setTable(){
-	 
+	public void createTable(){
 		table.setRowCount(numRows+1);
 		table.setRowCount(manager.getList().size());
 	    dataProvider.addDataDisplay(table);
 	    table.setSelectionModel(selectionModel);
+	    ids = table.getRowCount();
 	    RootPanel.get().add(table);
-	    ids = table.getRowCount();	
-	
-	    
 	}
-	
-
-	
+		
 	//(addButton,10,100);
 	public void addButton(){
-		
 		Button addButton = new Button("Add movie", new ClickHandler() {
 			public void onClick(ClickEvent event) {				
 				dataProvider.getList().addAll(numRows, manager.empty());
@@ -238,16 +200,6 @@ public class MovieUI extends Composite {
 	//(tbox,210,100);
 	public void setTextBox(){
 	      RootPanel.get().add(tbox,210,100);
-	}
-	
-	// NULL
-	public void setListBox(){
-		lbox.addItem("German");
-		lbox.addItem("English");
-		lbox.addItem("Spanish");
-		lbox.addItem("France");
-		panel.add(lbox);
-		
 	}
 	
 	public void show() {
