b518b0f3c87019e90849d8796522b9e0adf7625f
Tano <Tano@Tano.Speedport_W_724V_Typ_A_05011603_00_007>
2015-11-05 22:47:34 +0100

    NDA > CLONE

---
+++ blatt4/src/de/unihd/movies/client/service/MovieManagerService.java
@@ -0,0 +1,28 @@
+package de.unihd.movies.client.service;
+
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import de.unihd.movies.client.Movie;
+
+@RemoteServiceRelativePath("bookservice")
+public interface MovieManagerService extends RemoteService {
+
+	/**
+	 * Loads all {@link Movie} from the server.
+	 * 
+	 * @return A list containing all books.
+	 * */
+	public ArrayList<Movie> loadMovies();
+
+	/**
+	 * Saves the given list of books on the server.
+	 * 
+	 * @param books
+	 *            The list of books to save.
+	 * */
+	public void saveMovies(ArrayList<Movie> books);
+
+}
